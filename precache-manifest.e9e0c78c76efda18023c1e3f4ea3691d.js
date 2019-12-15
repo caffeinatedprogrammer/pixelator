c7c074892a6fbd207480dc6faa75e5db6e1cef01
Tony Chow <tony@Tonys-MacBook-Air.local>
2019-12-15 19:55:06 +0800

    Updates

---
+++ precache-manifest.e9e0c78c76efda18023c1e3f4ea3691d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "099d9a0d2ad535cd2a26944b92c0dac0",
+    "url": "/Pixelator/index.html"
+  },
+  {
+    "revision": "f0ddfb6e3532bc970840",
+    "url": "/Pixelator/static/css/main.ad94b372.chunk.css"
+  },
+  {
+    "revision": "2fecf95e32006a930dff",
+    "url": "/Pixelator/static/js/2.ab7637cf.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Pixelator/static/js/2.ab7637cf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f0ddfb6e3532bc970840",
+    "url": "/Pixelator/static/js/main.3c30bccc.chunk.js"
+  },
+  {
+    "revision": "ee59915fc96d2ff1255e",
+    "url": "/Pixelator/static/js/runtime-main.2348a86d.js"
+  }
+]);
