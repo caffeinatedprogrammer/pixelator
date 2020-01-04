2f51295c89def9865e3d225236bd15fb0ad73207
Caffeinated Programmer <caffeinatedprogrammer@outlook.com>
2020-01-04 21:28:21 +0800

    Updates

---
+++ precache-manifest.22a6dc932102eaad4ac09146ddbd8fd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2f8c4b5a7194bc897afce6aee040cb2",
+    "url": "/Pixelator/index.html"
+  },
+  {
+    "revision": "a156ec2e996d7411e53e",
+    "url": "/Pixelator/static/css/main.a4ae3c9b.chunk.css"
+  },
+  {
+    "revision": "80d3f100f3fd1e2394e3",
+    "url": "/Pixelator/static/js/2.7bb4d62d.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Pixelator/static/js/2.7bb4d62d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a156ec2e996d7411e53e",
+    "url": "/Pixelator/static/js/main.87af9cbe.chunk.js"
+  },
+  {
+    "revision": "ee59915fc96d2ff1255e",
+    "url": "/Pixelator/static/js/runtime-main.2348a86d.js"
+  }
+]);
