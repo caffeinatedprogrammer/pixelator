5d2e88cce0168e42e4ffe3bd65db76d1f9d70cfd
Caffeinated Programmer <caffeinatedprogrammer@outlook.com>
2020-03-14 17:08:25 +0800

    Add License in About page

--- src/AboutPage.jsx
+++ src/AboutPage.jsx
@@ -2,6 +2,8 @@ import React from "react";
 
 import { useTitle } from './hooks';
 
+import "./AboutPage.css";
+
 export default function AboutPage() {
     useTitle('Pixelator | About');
     
@@ -16,6 +18,20 @@ export default function AboutPage() {
                 <p>
                     The source code is release under the MIT License.
                 </p>
+                <div className="license">
+                    <p className="mono">
+                        Copyright 2020 Caffeinated Programmer
+                    </p>
+                    <p className="mono">
+                        Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+                    </p>
+                    <p className="mono">
+                        The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+                    </p>
+                    <p className="mono">
+                        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+                    </p>
+                </div>
             </div>
         </div>
     )
